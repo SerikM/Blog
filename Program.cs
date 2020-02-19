c19bd31bd1722e7e961d7124c89abe8928f2ef03
SerikM <tyrkamotchka@mail.ru>
2020-02-19 13:05:02 +1100

    n/m

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Blog
+namespace BlogReact
 {
     public class Program
     {
